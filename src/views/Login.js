22a0787db4b52f03ae383dd03041d79caecc60b1
Bartosz Legięć <bartosz@legiec.eu>
2018-06-24 10:40:17 +0200

    #1

---
+++ src/views/Login.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    }
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { username, password } = this.state;
+
+    console.log(username, password);
+  }
+
+  onChangeUsername = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangePassword = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Login</h1>
+        <input  
+          type="text"
+          placeholder="username"
+          onChange={this.onChangeUsername}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={this.onChangePassword} 
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
+export default Login;
